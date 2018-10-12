86571ac789a3b709da9e9d55a19dab1d59bd9a30
Alper Silistre <alper.silistre@broadangle.com>
2018-10-12 19:29:15 +0300

    Project files added to the repository.

---
+++ RestaurantAspNetCore/Services/IRestaurantData.cs
@@ -0,0 +1,10 @@
+﻿using RestaurantAspNetCore.Models;
+using System.Collections.Generic;
+
+namespace RestaurantAspNetCore.Services
+{
+	public interface IRestaurantData
+    {
+		IEnumerable<Restaurant> GetAll();
+    }
+}
---
+++ RestaurantAspNetCore/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantAspNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAspNetCore.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+		List<Restaurant> restaurants;
+
+		public InMemoryRestaurantData()
+		{
+			restaurants = new List<Restaurant>
+			{
+				new Restaurant { Id = 1, Name = "Alper's place" },
+				new Restaurant { Id = 2, Name = "Hovel restaurant" },
+				new Restaurant { Id = 3, Name = "King's Contrivance" },
+			};
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			return restaurants.OrderBy(r => r.Name);
+		}
+	}
+}
