a5b4575ddfc5680cdad1f9b9e83f96dae70feb95
Alper Silistre <alpersilistre@gmail.com>
2018-10-13 19:54:40 +0300

    IGreeter service added to the HomeController to get the message of the day. HomeViewIndexModel created instead of using Entity Model in view.

--- RestaurantAspNetCore/Controllers/HomeController.cs
+++ RestaurantAspNetCore/Controllers/HomeController.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestaurantAspNetCore.Models;
 using RestaurantAspNetCore.Services;
+using RestaurantAspNetCore.ViewModels;
 
 namespace RestaurantAspNetCore.Controllers
 {
 	public class HomeController : Controller
     {
 		private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-		public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
 		{
 			_restaurantData = restaurantData;
+            _greeter = greeter;
 		}
 
 		public IActionResult Index()
 		{
-			var model = _restaurantData.GetAll();
-
+            var model = new HomeIndexViewModel
+            {
+                Restaurants = _restaurantData.GetAll(),
+                CurrentMessage = _greeter.GetMessageOfTheDay()
+            };
+            
 			return View(model);
 		}
     }
---
+++ RestaurantAspNetCore/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantAspNetCore.Models;
+using System.Collections.Generic;
+
+namespace RestaurantAspNetCore.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public string CurrentMessage { get; set; }
+    }
+}
