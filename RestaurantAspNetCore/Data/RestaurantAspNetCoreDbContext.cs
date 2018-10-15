b36aa071400fd5a656aa71bd71d069a4b8da702e
alpersilistre <alpersilistre@gmail.com>
2018-10-15 18:16:02 +0300

    Local sql server created for the project and ef core added.

---
+++ RestaurantAspNetCore/Data/RestaurantAspNetCoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantAspNetCore.Models;
+
+namespace RestaurantAspNetCore.Data
+{
+	public class RestaurantAspNetCoreDbContext : DbContext
+    {
+		public RestaurantAspNetCoreDbContext(DbContextOptions options) 
+			: base(options)
+		{
+
+		}
+
+		public DbSet<Restaurant> Restaurants { get; set; }
+	}
+}
---
+++ RestaurantAspNetCore/Services/SqlRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RestaurantAspNetCore.Data;
+using RestaurantAspNetCore.Models;
+
+namespace RestaurantAspNetCore.Services
+{
+	public class SqlRestaurantData : IRestaurantData
+	{
+		private RestaurantAspNetCoreDbContext _context;
+
+		public SqlRestaurantData(RestaurantAspNetCoreDbContext context)
+		{
+			_context = context;
+		}
+
+		public Restaurant Add(Restaurant restaurant)
+		{
+			_context.Restaurants.Add(restaurant);
+			_context.SaveChanges();
+
+			return restaurant;
+		}
+
+		public Restaurant Get(int id)
+		{
+			return _context.Restaurants.FirstOrDefault(x => x.Id == id);
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			return _context.Restaurants.OrderBy(x => x.Name);
+		}
+	}
+}
